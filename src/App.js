65e05f44021ddb794dfbbcb3ffbd3d7445d11153
Rosario Pittaro <rosario.pittaro1105@gmail.com>
2023-01-09 09:46:31 -0300

    Se agrego LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Formulario  from "./components/Formulario";
 import Cita  from "./components/Cita";
 
@@ -6,9 +6,25 @@ import Cita  from "./components/Cita";
 
 function App() {
 
+
+  //Citas en LOCAL STORAGE
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales) {
+    citasIniciales =[];
+  }
+
 //Arreglo de todas las citas
 
 const [citas, guardarCitas] = useState([]);
+
+//useEffect para realizar ciertas operaciones cuando el state cambia
+useEffect( () => {
+if(citasIniciales){
+  localStorage.setItem('citas', JSON.stringify(citas))
+} else{
+  localStorage.setItem('citas', JSON.stringify([]));
+}
+}, [citas] );
 
 //Funcion que tome las citas anuales y agregue la nueva
 const crearCita = cita => {
